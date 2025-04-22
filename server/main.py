36b4443e03266b223c5e115671acc001ea966200
Gutha Prathyush <prathyushgutha@gmail.com>
2025-04-23 00:19:25 +0530

    preparing for deployment

--- server/main.py
+++ server/main.py
@@ -36,5 +36,5 @@ async def root(new_message: Message):
     return Message(role='assistant', content=response)
 
 if __name__ == '__main__':
-    PORT = int(os.getenv("PORT", 8000))
+    PORT = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=PORT)
